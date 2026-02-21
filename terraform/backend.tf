20fdcfe9ef6fe0a0faa1389b3d59a29bd1974671
Perez9070 <perezmainaabel@gmail.com>
2026-02-21 06:00:13 +0300

    Update Terraform infra and CI pipeline

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     bucket         = "bedrock-terraform-state-alt-soe-025-1334"
-    key            = "project-bedrock/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "bedrock-terraform-locks-alt-soe-025-1334"
+    use_lockfile   = true  # ← New way (S3 native locking)
     encrypt        = true
   }
 }
