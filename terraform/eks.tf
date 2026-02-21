c8aa63444e103c8d66efdd96da766f7259685ae6
Perez9070 <perezmainaabel@gmail.com>
2026-02-21 05:11:29 +0300

    Update Terraform infra and CI pipeline

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0" #or "21.0"
+  version = "~> 21.0" #"~> 20.0" 
 
   cluster_name    = var.cluster_name
   cluster_version = "1.34"
